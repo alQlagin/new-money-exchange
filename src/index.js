0d27e0344de3d6505f9dd2620d4d43df5bac722a
Alex Kulagin <al.qlagin@gmail.com>
2019-07-02 04:05:11 +0500

    added realization for makeExchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 // PLEASE DON'T change function name
+const FACE_VALUES = [
+  {key: 'H', value: 50},
+  {key: 'Q', value: 25},
+  {key: 'D', value: 10},
+  {key: 'N', value: 5},
+  {key: 'P', value: 1},
+];
+
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+  if (currency > 10000) {
+    return {error: 'You are rich, my friend! We don\'t have so much coins for exchange'}
+  }
+  const result = FACE_VALUES.reduce(accumulator, {rest: currency});
+  delete result.rest;
+  return result;
+};
+
+function accumulator(accumulated, faceValue) {
+  const value = Math.floor(accumulated.rest / faceValue.value);
+
+  if (value > 0) {
+    accumulated[faceValue.key] = value;
+  }
+  accumulated.rest = accumulated.rest % faceValue.value;
+
+  return accumulated;
 }
